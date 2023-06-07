ab6c0748b013e40183b0ad069f6a8fe807205518
MartheUwir <marthuwir@gmail.com>
2023-06-07 17:45:53 +0200

    error-like

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,5 @@
-#!/usr/bin/python3
-print("\"Programming is like building a multilingual puzzle")
+from __future__ import print_function
+
+print("""\
+"Programming is like building a multilingual puzzle"
+""")
