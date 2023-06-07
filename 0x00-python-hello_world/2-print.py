430581668c6cda6bd5a34e64d8370a155f8f5d20
MartheUwir <marthuwir@gmail.com>
2023-06-07 17:49:02 +0200

    error-like

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-
-"""
-This script demonstrates the analogy of programming to building a multilingual puzzle.
-"""
-
 print("\"Programming is like building a multilingual puzzle")
