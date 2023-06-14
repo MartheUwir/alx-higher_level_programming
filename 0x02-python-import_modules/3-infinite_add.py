93ec1f5080434a2401f96a196600a9b69dfbe056
MartheUwir <marthuwir@gmail.com>
2023-06-14 15:17:55 +0200

    upto

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    n = len(sys.argv)
-    sum = 0
-    for i in range(1, n):
-        sum += int(sys.argv[i])
-    print(sum)
+    result = 0
+    import sys
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if "__" not in name:
-            print(name)
+    import hidden_4
+    dir(hidden_4)
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
